777736add8741e03903b72d51feac9d8c16ce166
unknown <kaciehawkins1@gmail.com>
2021-08-16 13:22:33 -0600

    updated MusicTable.jsx and css

--- src/components/MusicTable.jsx
+++ src/components/MusicTable.jsx
@@ -11,20 +11,14 @@ const MusicTable = (props) => {
                     <th>artist</th>
                     <th>album</th>
                     <th>release_date</th>
-
-
-
-                    <td className="title">{props.songs.title}</td>
-                    <td className="artist">{props.songs.artist}</td>
-                    <td className="album">{props.songs.album}</td>
-                    <td className="release_date">{props.songs.release_date}</td>
-                </tr>
+                  </tr>
+                 
+                     {props.songs.map((song, index)=><tr key={index}><td className="music-table__title">{song.title}</td><td className="music-table__artist">{song.artist}</td><td>{song.album}</td><td>{song.release_date}</td></tr>)}
+                
+                
             </table>
-
-            <ul>
-                {props.songs.map((song, index) => <li key={index}>{song.title}{song.artist}
-                    {song.album}{song.release_date}</li>)}
-            </ul>
+           
+           
 
         </div>
     )
