9003db77d9edad6daeece20ec177e4da25ae8d03
unknown <kaciehawkins1@gmail.com>
2021-08-17 14:55:23 -0600

    fixed merge conflicts and added functions to searchbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,70 +19,59 @@ class App extends Component {
     }
 
     async getSongs() {
-        try{
+        try {
             let response = await axios.get('http://127.0.0.1:8000/music/');
             console.log(response)
             this.setState({
                 songs: response.data
             });
         }
-        catch(except){
+        catch (except) {
             alert('Not Valid')
         }
     }
 
-    deleteRow = async(id) => {
-        try{
-            await axios.delete('http://127.0.0.1:8000/music/${id}/')
-            this.getSongs()
-        }
-        catch(event){
-            console.log(event)
-        }
-    }
 
-<<<<<<< HEAD
-    filterResults = (field, searchWord) =>{
+
+    filterResults = (field, searchWord) => {
         console.log(field, searchWord)
-        let results = this.state.songs.filterResults(function(event){
-            if(event[field] == searchWord){
+        let results = this.state.songs.filterResults(function (event) {
+            if (event[field] == searchWord) {
                 return true
             }
         })
         this.setState({
             songs: results
         })
-=======
-    deleteRow = (id) => {
-        axios.delete(`http://127.0.0.1:8000/music/${id}/`)
-        .then(() => this.setState({ status: 'Delete Complete'}))
-        window.location.reload();
->>>>>>> c0bd3f8d8f05923f9c53b9918a864c4aa81c31ae
     }
+
+        deleteRow = (id) => {
+            axios.delete(`http://127.0.0.1:8000/music/${id}/`)
+                .then(() => this.setState({ status: 'Delete Complete' }))
+            window.location.reload();
+        }
+    
 
     songCreator = (newSong) => {
         this.state.songs.push(newSong);
         axios.post('http://127.0.0.1:8000/music/', newSong)
-        .then(response => this.setState=({
-            newSong: response.data
-        }));
+            .then(response => this.setState = ({
+                newSong: response.data
+            }));
         window.location.reload();
     }
-    
+
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 <SearchBar />
-                <MusicTable songs ={this.state.songs} filter={this.filterResults} songs={this.state.songs} />
+                <MusicTable songs={this.state.songs} filter={this.filterResults} songs={this.state.songs} delete={this.deleteRow} />
                 <SongForm createNewSong={this.songCreator.bind(this)} />
-=======
-                 <MusicTable songs ={this.state.songs} delete={this.deleteRow}/>
-                 <SongForm createNewSong={this.songCreator.bind(this)} />
->>>>>>> c0bd3f8d8f05923f9c53b9918a864c4aa81c31ae
+
+
             </div>
-        );
+        )
     }
 }
 
-export default App
+export default App;
